a1b21f125e7628722d3c1f6d68191efd3ee7907b
zwk <zhangweikun@yijianar.com>
2022-11-03 20:22:22 +0800

    feat: 更改配置类为注解

---
+++ src/main/java/yj/annotation/EnableMappingAutoConfigurer.java
@@ -0,0 +1,22 @@
+package yj.annotation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import yj.processor.MappingPermissionListener;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 收集系统中的url及权限信息
+ * 1、配置文件中添加 spring.application.name 配置
+ * 2、实现{@link yj.processor.MappingPostProcessor} 接口
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Configuration
+@Import(MappingPermissionListener.class)
+public @interface EnableMappingAutoConfigurer {
+}
